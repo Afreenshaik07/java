e008f2207e34e71c6cb83fa248fe24be8bb1d6a3
Afreen Shaik <afreenshaik0711@gmail.com>
2025-05-25 07:51:42 +0530

    Add files via upload

---
+++ Std_detail.java
@@ -0,0 +1,19 @@
+class Student
+{
+String name;
+int roll;
+Student()
+{
+name="abc";
+roll=123;
+System.out.println("Student name is"+name);
+System.out.println("Student roll number is"+roll);
+}
+}
+class Std_detail
+{
+public static void main(String args[])
+{
+Student s1=new Student();
+}
+}
