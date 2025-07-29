0a9f3ea83fe53be528c1bacf200bce9bfddd158f
Afreen Shaik <afreenshaik0711@gmail.com>
2025-07-29 08:10:27 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,14 @@
+class Escape
+{
+public static void main(String args[])
+{
+System.out.ptintln("CSEDS\n RGMCET");
+System.out.ptintln("CSEDS\t RGMCET");
+System.out.ptintln("CSEDS \\ RGMCET");
+System.out.ptintln("CSEDS\'RGMCET");
+System.out.ptintln("CSEDS\'RGMCET\'");
+SySystem.out.ptintln("CSEDS\"RGMCET");
+System.out.ptintln("CSEDS\"RGMCET\"");
+System.out.ptintln("CSEDS\bRGMCET");
+}
+}
