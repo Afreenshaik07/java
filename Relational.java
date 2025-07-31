39e9c489d34d7eaff17b130c6193ab93356a8477
Afreen Shaik <afreenshaik0711@gmail.com>
2025-07-31 18:37:03 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Relational
+{
+public static void main(String args[])
+{
+int n1=2,n2=1;
+//Scanner object=new Scanner(System.in);
+//System.out.print("enter number1:");
+//n1=object.nextInt();
+//System.out.print("enter number2:");
+//n2=object.nextInt();
+System.out.println("greater of two numbers:"+(n1>n2));
+System.out.println("lessthan of two numbers:"+(n1<n2));
+System.out.println("greaterthan or equal of two numbers:"+(n1>=n2));
+System.out.println("lesserthan or equal of two numbers:"+(n1<=n2));
+System.out.println("equal of two numbers:"+(n1==n2));
+System.out.println("notequal of two numbers:"+(n1!=n2));
+
+}
+}
+
+
