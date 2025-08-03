99d43f27a945bdbf26a8c646fb4cbf69409944cf
Afreen Shaik <afreenshaik0711@gmail.com>
2025-08-03 10:54:54 +0530

    Add files via upload

---
+++ Condition3.java
@@ -0,0 +1,21 @@
+class Condition3
+{
+public static void main(String args[])
+{
+int n=34;
+if(n>=0)
+{
+System.out.println("it is a positive number");
+if(n%2==0)
+{
+System.out.print("it is even number");
+}
+else
+{
+System.out.println("it is odd number");
+}
+}
+else
+System.out.println("it is a negative number");
+}
+}
